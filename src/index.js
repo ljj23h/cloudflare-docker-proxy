2bf70c8555f125bef553becb437238bb69064980
ljj23h <ljj23h@gmail.com>
2024-07-10 16:45:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.526828.xyz": dockerHub,
+  "quay.526828.xyz": "https://quay.io",
+  "gcr.526828.xyz": "https://gcr.io",
+  "k8s-gcr.526828.xyz": "https://k8s.gcr.io",
+  "k8s.526828.xyz": "https://registry.k8s.io",
+  "ghcr.526828.xyz": "https://ghcr.io",
+  "cloudsmith.526828.xyz": "https://docker.cloudsmith.io",
+  "ecr.526828.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.526828.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
